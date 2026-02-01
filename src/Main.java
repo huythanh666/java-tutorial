9ae41846ff323e67ef1ae1d24ccbde7906835cd1
nguyenhuythanh <dev.nguyenhuythanh@gmail.com>
2026-02-01 17:06:22 +0700

    learn how java work

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,9 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
-
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.print("Day 1\n");
+        System.out.print("sout \n");
     }
 }
+/* println thiết kế theo kiểu Overloading(nạp chồng phương thức)
+ vd println(int x) khi bỏ vào biến x vào print java sẽ tự động tìm phương thức phù hợp */
