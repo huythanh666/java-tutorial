5417d992aed3fd2757a72cbdd7f0805307e3e683
nguyenhuythanh <dev.nguyenhuythanh@gmail.com>
2026-02-03 15:09:00 +0700

    rectangle

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
        Scanner scanner = new Scanner(System.in);
+       double width = 0;
+       double height = 0 ;
+       double area = 0;
 
-        System.out.print("Enter ur grade: ");
-        int grade = scanner.nextInt();
-        scanner.nextLine();
+        System.out.print("Enter width: ");
+        width = scanner.nextDouble();
+        System.out.print("Enter height: ");
+        height = scanner.nextDouble();
+        area = width * height;
+        System.out.println("The area is " + area + "cm²");
 
-        System.out.print("Enter ur name: ");
-        String name = scanner.nextLine();
-
-        System.out.print("Enter ur gpa: ");
-        double gpa = scanner.nextDouble();
-
-        System.out.print("Are u a student: ");
-        boolean isStudent = scanner.nextBoolean();
-
-        System.out.println("Hello " + name);
-        System.out.println("Your grade is " + grade);
-        System.out.println("Your gpa is " + gpa);
-        if(isStudent) System.out.println("You are a enroll as a student");
-        else{
-            System.out.println("You are not enroll");
-        }
        scanner.close();
     }
 }
