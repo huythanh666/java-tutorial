24e7063ad1de8670846b5874d6bfdbb7fe7e0c85
nguyenhuythanh <dev.nguyenhuythanh@gmail.com>
2026-02-03 15:28:38 +0700

    create mad libs game

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,28 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-       Scanner scanner = new Scanner(System.in);
-       double width = 0;
-       double height = 0 ;
-       double area = 0;
+        Scanner scanner = new Scanner(System.in);
+        String adj1;
+        String adj2;
+        String adj3;
+        String noun1;
+        String verb1;
 
-        System.out.print("Enter width: ");
-        width = scanner.nextDouble();
-        System.out.print("Enter height: ");
-        height = scanner.nextDouble();
-        area = width * height;
-        System.out.println("The area is " + area + "cm²");
+        System.out.print("Enter an adj");
+        adj1 = scanner.nextLine();
+        System.out.print("Enter a noun (animal or person)");
+        noun1 = scanner.nextLine();
+        System.out.print("Enter an adj");
+        adj2 = scanner.nextLine();
+        System.out.print("Enter a verb (action)");
+        verb1 = scanner.nextLine();
+        System.out.print("Enter an adj");
+        adj3 = scanner.nextLine();
 
-       scanner.close();
+        System.out.println("Today i went to a " + adj1 + " zoo.");
+        System.out.println("In an exhibit, i saw a " + noun1 + ".");
+        System.out.println(noun1 + " was " + adj2 + " and " + verb1 + "!");
+        System.out.println("I was " + adj3 + "!");
+        scanner.close();
     }
 }
