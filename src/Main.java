f98ffbce59609acfafd8563ec05d1449a66e98f9
nguyenhuythanh <dev.nguyenhuythanh@gmail.com>
2026-02-04 16:30:51 +0700

    shopping

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,21 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int x = 10;
-        int y = 2;
-        int z = 0;
-        z = x+y;
-        x+=y;
-        x-=y;
-        x/=y;
-        x*=y;
-        x%=y;
-        x++;
-        y--;
-        System.out.println(z);
-        System.out.println(x);
+        Scanner scanner = new Scanner(System.in);
+        String item;
+        double price;
+        int quantity;
+        char currency = '$';
+        double total;
+        System.out.print("What item would u like to buy?: ");
+        item = scanner.nextLine();
+        System.out.print("What is the price for each?: ");
+        price = scanner.nextDouble();
+        System.out.print("How many would u like?: ");
+        quantity = scanner.nextInt();
+        total = price * quantity;
+        System.out.println("u need to pay " + total + currency);
+        scanner.close();
+
     }
 }
