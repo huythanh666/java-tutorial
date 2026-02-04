c146f3cab5394e66542861f11697c5605accfe22
nguyenhuythanh <dev.nguyenhuythanh@gmail.com>
2026-02-04 17:19:10 +0700

    learn if else if

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,16 @@
 import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
+public class Main{
+    public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        String item;
-        double price;
-        int quantity;
-        char currency = '$';
-        double total;
-        System.out.print("What item would u like to buy?: ");
-        item = scanner.nextLine();
-        System.out.print("What is the price for each?: ");
-        price = scanner.nextDouble();
-        System.out.print("How many would u like?: ");
-        quantity = scanner.nextInt();
-        total = price * quantity;
-        System.out.println("u need to pay " + total + currency);
-        scanner.close();
-
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+        if (age >=18){
+            System.out.println("U are an adult");
+        }else if(age < 0 ){
+            System.out.println("U haven't been born yet!");
+        }
+        else{
+            System.out.println("U are a child!");
+        }
     }
 }
