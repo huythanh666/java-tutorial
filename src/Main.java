ef6405d665574f5ece1eefcbab76aa2dc74a2471
nguyenhuythanh <dev.nguyenhuythanh@gmail.com>
2026-02-06 22:54:50 +0700

    learn Math

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 import java.util.Random;
 public class Main{
     public static void main(String[] args){
-         Random random = new Random();
-         int number;
-         number = random.nextInt();
-         System.out.println(number);
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+        double result;
+        result = Math.pow(2,5);
+        result = Math.abs(-5);
+        result = Math.sqrt(9);
+        result = Math.round(3.4); // làm tròn đến số gần nhất
+        result = Math.floor(3.99); // làm tròn dưới
+        result = Math.ceil(3.1); // làm tròn trên
+        System.out.println(result);
     }
 }
