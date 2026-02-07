23f6121fe0c2d6de00e78a80ccda3ce1a01d09e6
nguyenhuythanh <dev.nguyenhuythanh@gmail.com>
2026-02-07 22:52:35 +0700

    use printf

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,25 @@
-import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        double a,b,c;
-        System.out.print("Enter the length of side a: ");
-        a = scanner.nextDouble();
-        System.out.print("Enter the length of side b: ");
-        b = scanner.nextDouble();
+        String name = "Elvin";
+        char firstLetter = 'E';
+        int age = 28;
+        double height = 60.5;
+        boolean isEmployed = true;
 
-        c = Math.sqrt(Math.pow(a,2)+Math.pow(b,2));
-
-        System.out.println("The hypotenuse is:  "+c + " cm²");
-        scanner.close();
+//        System.out.printf("Hello %s\n", name);
+//        System.out.printf("Your name starts with a %c\n", firstLetter);
+//        System.out.printf("You are %d years old\n", age);
+//        System.out.printf("You are %f inches tall\n", height);
+//        System.out.printf("Employed %b\n", isEmployed);
+//        System.out.printf("%s is %d years old\n", name, age);
+        double price = 1900.99;
+        /*
+        * + thêm dấu cộng vào trước số
+        * , ngăn cách số vd 19000 = 19,000
+        * ( số  âm sẽ được ngăn cách trong ()
+        * space hiển thị dấu trừ với số âm, dấu cách với số dương
+        * */
+        System.out.printf("%.4f\n",height);
+        System.out.printf("%+.1f\n",price);
     }
 }
