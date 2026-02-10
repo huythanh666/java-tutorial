70543111f0a675d469473b1404dc3bc6805d74a9
nguyenhuythanh <dev.nguyenhuythanh@gmail.com>
2026-02-11 00:20:28 +0700

    use static

--- src/Car.java
+++ src/Car.java
@@ -1,11 +1,16 @@
 public class Car {
+    static int numberOfCar;
     String model;
     String color;
     Car(String model, String color){
         this.model = model;
         this.color= color;
+        numberOfCar++;
     }
     void drive(){
         System.out.println("You drive the " + this.color + " " + this.model);
+    }
+    static void showCar(){
+        System.out.println("You have " + numberOfCar + " car");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,15 @@ public class Main{
         Car car1 = new Car("Ford","Black");
         Car car2 = new Car("Mustang","Red");
         Car car3 = new Car("Mayback","Blue");
-//      Car[] cars = {car1,car2,car3};
-        Car[] cars = {
-                 new Car("Ford","Black"),
-                 new Car("Mustang","Red"),
-                 new Car("Mayback","Blue")
-        };
+      Car[] cars = {car1,car2,car3};
+//        Car[] cars = {
+//                 new Car("Ford","Black"),
+//                 new Car("Mustang","Red"),
+//                 new Car("Mayback","Blue")
+//        };
         for(Car car: cars){
             car.drive();
         }
+        Car.showCar();
     }
 }
