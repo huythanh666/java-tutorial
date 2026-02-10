da692e179900ba9f1a311c06ae2d1dd635314726
nguyenhuythanh <dev.nguyenhuythanh@gmail.com>
2026-02-10 18:54:01 +0700

    use constructor

--- src/Main.java
+++ src/Main.java
@@ -2,29 +2,8 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args) throws InterruptedException {
-      String[] question = {"1+1=?","2+2=?","5+5=?"};
-      String[][] options = {{"1. 2","2. 4", "3. 7"},
-                            {"1. 2","2. 4", "3. 7"},
-                            {"1. 10","2. 4", "3. 7"}
-      };
-      Scanner scanner = new Scanner(System.in);
-        int guess;
-        int score = 0;
-        int[] answer = {1,2,1};
-        for (int i = 0; i < question.length; i++) {
-            System.out.println(question[i]);
-            for(String option: options[i]){
-                System.out.println(option);
-            }
-            System.out.print("Enter your answer: ");
-            guess = scanner.nextInt();
-            if(guess == answer[i]){
-                score++;
-                System.out.println("correct");
-            }else{
-                System.out.println("wrong");
-            }
-        }
-        System.out.println("Điểm số là: " + score +"/3");
+        Student student = new Student("thanh",18,4.0);
+        System.out.println(student.name);
+        student.study();
     }
 }
