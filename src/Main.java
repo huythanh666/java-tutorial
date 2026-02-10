03f76aff55328d5376536072b396e89688f2630a
nguyenhuythanh <dev.nguyenhuythanh@gmail.com>
2026-02-10 22:29:16 +0700

    use array object

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
-import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args) throws InterruptedException {
-        Student student = new Student("thanh",18,4.0);
-        System.out.println(student.name);
-        student.study();
+        Car car1 = new Car("Ford","Black");
+        Car car2 = new Car("Mustang","Red");
+        Car car3 = new Car("Mayback","Blue");
+//      Car[] cars = {car1,car2,car3};
+        Car[] cars = {
+                 new Car("Ford","Black"),
+                 new Car("Mustang","Red"),
+                 new Car("Mayback","Blue")
+        };
+        for(Car car: cars){
+            car.drive();
+        }
     }
 }
